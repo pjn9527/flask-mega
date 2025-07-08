21ad8cbc80817ad31e4baec49ca42be5abc9e8bf
pjn9527 <576716309@qq.com>
2025-07-08 13:16:58 +0800

    完善重置密码邮件，提供中文翻译

--- app/email.py
+++ app/email.py
@@ -3,10 +3,15 @@ from app import mail, app
 from flask import render_template
 from threading import Thread
 from flask_babel import _
-
+from flask_babel import force_locale
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
+    with force_locale('zh'):
+        html_body = render_template('email/reset_password.html',
+                                    user=user, token=token)
+        text_body = render_template('email/reset_password.txt',
+                                    user=user, token=token)
     send_email(
         _('[8 mile road] Reset Your Password'),
         sender=app.config['ADMINS'][0],
